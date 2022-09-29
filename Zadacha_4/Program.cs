3e5454f6e9f908568831e5ef525ed83e256541aa
Anna IT <89269690212@mail.ru>
2022-09-29 20:57:25 +0300

    Решение четвертой задачи

---
+++ Zadacha_4/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine ("Введите число: ");
+
+int number = Convert.ToInt32(Console.ReadLine());
+int start_number = 1;
+
+while (start_number <= number)
+{
+    if (start_number % 2 ==0 )
+    Console.Write (start_number + ", ");
+    start_number ++;
+}
