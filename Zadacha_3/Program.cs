6a975f506d9c2009ae5e2ab3f83bfe1995130f6f
Anna IT <89269690212@mail.ru>
2022-09-29 20:31:11 +0300

    Решение третьей задачи

---
+++ Zadacha_3/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine ("Введите число: ");
+
+int number = 7;
+
+if (number % 2 == 0)
+{
+    Console.WriteLine ("Четное число ");
+}
+else
+{
+    Console.WriteLine ("Нечетное число ");
+}
