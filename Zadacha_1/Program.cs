e65f670bc6df9a650ed827e5083ec0352ff6b1ff
Anna IT <89269690212@mail.ru>
2022-09-29 19:41:57 +0300

    Первая задача

---
+++ Zadacha_1/Program.cs
@@ -0,0 +1,23 @@
+﻿int a = -9;
+int b = -3;
+
+int max = a;
+int min = b;
+
+if (a > b)
+{ 
+    max = a;
+    min = b;
+}
+if (b > a)
+{ 
+    max = b;
+    min = a;
+}
+
+Console.Write ("Max number ");
+Console.WriteLine (max);
+
+Console.Write ("Min number ");
+Console.WriteLine (min);
+
